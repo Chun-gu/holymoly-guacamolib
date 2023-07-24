e7fe48afd369eb38d84f9d0e9466779724ce4626
Chun-gu <chungu.yi92@gmail.com>
2023-07-24 17:58:40 +0900

    config: custom theme for screens and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,28 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    screens: {
+      sm: { max: '767px' },
+      md: { min: '768px', max: '1023px' },
+      lg: { min: '1024px' },
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      yellow: { light: '#fdfad1' },
+      green: { disabled: '#9ad9b0', light: '#5fe18c', DEFAULT: '#38af61' },
+      white: '#ffffff',
+      grey: {
+        100: '#f5f5f5',
+        200: '#f3f3f3',
+        300: '#e9e9e9',
+        400: '#e3e3e3',
+        500: '#d6d6d6',
+        600: '#c9c9c9',
+        700: '#b9b9b9',
+        800: '#7f7f7f',
       },
+      black: '#000000',
     },
   },
   plugins: [],
