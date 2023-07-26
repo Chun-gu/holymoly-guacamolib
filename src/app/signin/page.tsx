f3ae36fa150541bb79f92782a1f1311362b444a5
Chun-gu <chungu.yi92@gmail.com>
2023-07-26 19:10:56 +0900

    feat: redirect signed-in user to `/`

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,5 +1,10 @@
 import SignInButton from '@/components/SignInButton'
+import { getServerSession } from 'next-auth'
+import { redirect } from 'next/navigation'
 
-export default function SignInPage() {
+export default async function SignInPage() {
+  const session = await getServerSession()
+  if (session?.user) return redirect('/')
+
   return <SignInButton />
 }
