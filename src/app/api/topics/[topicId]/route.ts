fc6dd2c4565732f3019c5d4a186d3140f1cd3739
Chun-gu <chungu.yi92@gmail.com>
2023-07-29 16:36:27 +0900

    feat: add deleteTopic

--- src/app/api/topics/[topicId]/route.ts
+++ src/app/api/topics/[topicId]/route.ts
@@ -6,13 +6,37 @@ type Params = { params: { topicId: string } }
 
 const NOT_FOUND_ERROR_CODE_FROM_PRISMA = 'P2025'
 
+// 주제 상세 정보
 export async function GET(req: Request, { params: { topicId } }: Params) {
   try {
     const topic = await prisma.topic.findUniqueOrThrow({
       where: { id: Number(topicId) },
     })
 
-    return NextResponse.json({ topic }, { status: 200 })
+    return NextResponse.json(topic, { status: 200 })
+  } catch (error) {
+    if (
+      error instanceof PrismaClientKnownRequestError &&
+      error.code === NOT_FOUND_ERROR_CODE_FROM_PRISMA
+    ) {
+      return NextResponse.json({ error: 'Not Found' }, { status: 404 })
+    }
+
+    return NextResponse.json({ error }, { status: 500 })
+  }
+}
+
+// 주제 삭제
+export async function DELETE(req: Request, { params: { topicId } }: Params) {
+  try {
+    const deletedTopic = await prisma.topic.delete({
+      where: { id: Number(topicId) },
+    })
+
+    return NextResponse.json(
+      { deletedTopicId: deletedTopic.id },
+      { status: 200 }
+    )
   } catch (error) {
     if (
       error instanceof PrismaClientKnownRequestError &&
