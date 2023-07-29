d4df588ab2f417749518ec02a0e2f690f0600166
Chun-gu <chungu.yi92@gmail.com>
2023-07-29 17:52:54 +0900

    feat: add GET route handler  for reading comments of comment

--- src/app/api/comments/[commentId]/comments/route.ts
+++ src/app/api/comments/[commentId]/comments/route.ts
@@ -6,6 +6,30 @@ import { NextResponse } from 'next/server'
 import { ZodError } from 'zod'
 import { Params } from '../route'
 
+const DEFAULT_SKIP = 0
+const DEFAULT_TAKE = 5
+
+// 댓글의 대댓글 목록
+export async function GET(req: Request, { params: { commentId } }: Params) {
+  try {
+    const { searchParams } = new URL(req.url)
+    const skip = Number(searchParams.get('skip') || DEFAULT_SKIP)
+    const take = Number(searchParams.get('take') || DEFAULT_TAKE)
+
+    const subComments = await prisma.comment.findMany({
+      where: { parentCommentId: Number(commentId) },
+      include: { author: { select: { id: true, name: true } } },
+      orderBy: { createdAt: 'desc' },
+      skip,
+      take,
+    })
+
+    return NextResponse.json(subComments)
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 })
+  }
+}
+
 // 대댓글 작성
 export async function POST(req: Request, { params: { commentId } }: Params) {
   try {
