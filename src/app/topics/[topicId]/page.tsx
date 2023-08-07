03499386de5b82ea2228be5842d92524b2b0374c
Chun-gu <chungu.yi92@gmail.com>
2023-08-07 19:46:17 +0900

    feat: add page for detailed topic

---
+++ src/app/topics/[topicId]/page.tsx
@@ -0,0 +1,34 @@
+import TopicDetail from '@/components/TopicDetail'
+import CommentList from '@/components/CommentList'
+import CommentInput from '@/components/CommentInput'
+
+type Params = { params: { topicId: string } }
+
+// TODO: change to SSR or SSG for metadata
+// async function getTopic(topicId: string): Promise<Topic> {
+//   const res = await fetch(`${process.env.API_URL}/api/topics/${topicId}`, {
+//     cache: 'no-store',
+//   })
+//   if (!res.ok) {
+//     throw new Error('Failed to fetch')
+//   }
+//   const data: Topic = await res.json()
+//   return data
+// }
+
+export default async function TopicDetailPage({ params: { topicId } }: Params) {
+  const numericTopicId = Number(topicId)
+
+  return (
+    <main>
+      <section>
+        <TopicDetail topicId={numericTopicId} />
+      </section>
+      <section>
+        <h2>댓글</h2>
+        <CommentInput topicId={numericTopicId} />
+        <CommentList topicId={numericTopicId} />
+      </section>
+    </main>
+  )
+}
