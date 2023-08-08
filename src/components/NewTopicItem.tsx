5a52582951f1073f55b4377296aa65a76e85deb2
Chun-gu <chungu.yi92@gmail.com>
2023-08-08 10:56:42 +0900

    style: add style to new topic list

--- src/components/NewTopicItem.tsx
+++ src/components/NewTopicItem.tsx
@@ -6,14 +6,14 @@ type Props = { topic: Topic }
 
 export default function NewTopicItem({ topic }: Props) {
   return (
-    <div className="flex justify-between items-center w-full h-[100px] bg-white py-[18px] px-[17px] border border-green rounded-[20px]">
+    <div className="flex justify-between items-center w-full h-[58px] bg-white py-[16px] px-[21px] border border-green rounded-[20px]">
       <TopicCard.Root topic={topic}>
         <Link href={`/topics/${topic.id}`}>
-          <TopicCard.Title className="font-bold" />
+          <TopicCard.Title className="font-semibold text-[14px]" />
         </Link>
-        <div className="flex gap-2">
-          <TopicCard.VoteCount className="flex items-center gap-1" />
-          <TopicCard.CommentCount className="flex items-center gap-1" />
+        <div className="flex gap-3">
+          <TopicCard.VoteCount className="flex items-center gap-1 font-semibold text-[14px]" />
+          <TopicCard.CommentCount className="flex items-center gap-1 font-semibold text-[14px]" />
         </div>
       </TopicCard.Root>
     </div>
